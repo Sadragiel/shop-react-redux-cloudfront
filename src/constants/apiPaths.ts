389f66cd72571976d4e39dd9fe66487bf8e959fa
Danylo Hulko <danylo_hulko@ls2.io>
2021-10-04 23:49:41 +0300

    Pony import service intgration =)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   product: 'https://hsenxpcp6b.execute-api.eu-central-1.amazonaws.com/dev/products',
   init: 'https://hsenxpcp6b.execute-api.eu-central-1.amazonaws.com/dev/init-products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://9gnsxvlkwh.execute-api.eu-central-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
